2d14a457829400925c73cf009f7660d0f1c37529
jqssun <jqssun+git@googlemail.com>
2026-04-17 13:21:51 +0100

    fdroid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     defaultConfig {
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
         applicationId = "io.github.jqssun.maceditor"
     }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,14 +11,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://jitpack.io")
-        }
-        mavenLocal {
-            content {
-                includeGroup("io.github.libxposed")
-            }
-        }
     }
     versionCatalogs {
         create("libs")
